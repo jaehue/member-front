968c6b840009afd390470a31711de33bb4f55f5c
Jang Jaehue <jaehue@jang.io>
2019-01-12 01:56:22 +0800

    Change sessionStorage to localStorage

--- src/appData.js
+++ src/appData.js
@@ -4,5 +4,5 @@ const appData = {
 
 export const setTeachers = (teachers) => appData.teachers = teachers
 export const getTeacherById = (teacherId) => appData.teachers.find(t => t.id == teacherId)
-export const setToken = (token) => sessionStorage.setItem('token', token)
-export const getToken = _ => sessionStorage.getItem('token')
+export const setToken = (token) => localStorage.setItem('token', token)
+export const getToken = _ => localStorage.getItem('token')
